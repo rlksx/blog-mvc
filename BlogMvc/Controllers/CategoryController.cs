865aefce794994ca589abe73517d0c700bca3784
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-11-27 20:00:37 -0300

    about: using async and await methods

--- BlogMvc/Controllers/CategoryController.cs
+++ BlogMvc/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 using BlogMvc.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogMvc.Controllers;
 
@@ -7,7 +8,15 @@ namespace BlogMvc.Controllers;
 public class CategoryController : ControllerBase
 {
     /* API REST - endpoints com o mesmo nome do controller no plural em minusculo */
-    [HttpGet("categories")]
-    public IActionResult Get([FromServices] BlogDataContext context) 
-        => Ok(context.Categories.ToList());
+    [HttpGet("v1/categories")]
+    public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context) 
+        => Ok(await context.Categories.ToListAsync());
+    /* await - aguarda o método retornar para continuar a executar async */
+    
+    /* async - indica que as ações serão executadas assicronamente,
+     e só retornam Taks<T> */
+    
+    // [HtppGet("v2/categories")] /=> versionamento;
+    // public IActionResult Getv2([FromServices] BlogDataContext context) 
+    //     => Ok(context.Categories.ToList());
 }
