c3278dcdd23f17702262732d0bb735ce2ffa933f
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-11-27 21:33:02 -0300

    feat: PostAsyc in the category

--- BlogMvc/Controllers/CategoryController.cs
+++ BlogMvc/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 using BlogMvc.Data;
+using BlogMvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,13 +11,38 @@ public class CategoryController : ControllerBase
     /* API REST - endpoints com o mesmo nome do controller no plural em minusculo */
     [HttpGet("v1/categories")]
     public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context) 
-        => Ok(await context.Categories.ToListAsync());
+        => Ok(await context.Categories.ToListAsync()); // retorn todas as categorias
     /* await - aguarda o método retornar para continuar a executar async */
+
+    [HttpGet("v1/categories/{id:int}")]
+    public async Task<IActionResult> GetByIdAsync(
+        [FromRoute] int id,
+        [FromServices] BlogDataContext context)
+    {
+        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        if (category == null) return NotFound(); // ERROR 404
+        
+        return Ok(category);
+    }
+
+
+    [HttpPost("v1/categories/")]
+    public async Task<IActionResult> PostAsync(
+        [FromServices] BlogDataContext context,
+        [FromBody] Category model)
+    {
+        await context.Categories.AddAsync(model);
+        await context.SaveChangesAsync();
+
+        return Created($"v1/categories/{model.Id}", model);
+    }
+    
     
     /* async - indica que as ações serão executadas assicronamente,
-     e só retornam Taks<T> */
+     e só retornam Taks<T>
+    [HtppGet("v2/categories")] /=> versionamento;
+    public IActionResult Getv2([FromServices] BlogDataContext context) 
+        => Ok(context.Categories.ToList()); */
     
-    // [HtppGet("v2/categories")] /=> versionamento;
-    // public IActionResult Getv2([FromServices] BlogDataContext context) 
-    //     => Ok(context.Categories.ToList());
+    /* await usando quando é o banco é acessado */
 }
